4a84ea8f6dd654d18745eff0037ee8d02943022a
u24957102 <u24957102@tuks.co.za>
2025-05-20 20:41:55 +0200

    edit home.php and home.css

--- PHP/home.php
+++ PHP/home.php
@@ -190,7 +190,7 @@
                     </button>
                 </div>
             </div>   
-                        <div class="col-4">
+            <div class="col-4">
                 <img src="..\image\toaster.jpg">
                 <h4 id="Title">Toaster</h4>
                 <div class="retail">
@@ -465,10 +465,10 @@
         </div>
 
     </div>
+    <script src="..\JS\home.js"></script>
     <?php
     include 'footer.php';
     ?>
-
 </body>
 
 </html>
